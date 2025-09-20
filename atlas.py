cce35f85d0829ef60412f3bb9a1a857746c822a7
Aditya B <adityabandewar21@gmail.com>
2025-09-20 09:59:44 +0530

    Initial clean

--- atlas.py
+++ atlas.py
@@ -4,4 +4,4 @@ def get_mongo_uri():
     """Reads MongoDB URI from config.json and returns it"""
     with open("config.json") as f:
         config = json.load(f)
-    return config["MONGO_URI1"]
+    return config["MONGO_URI"]
